8ffe1bd10c724768744e986a897587786f8d5ed4
rooney <bch3454@gmail.com>
2025-12-26 17:28:04 +0900

    feat: 프로젝트 초기 세팅
    - nextjs
    - shadcn/ui

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+      Page
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
+};
+
+export default Page;
